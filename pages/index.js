82ef68756ee8ff0b5312abba3734953107aae3d5
VuongVX <vuongvx@hotmail.com>
2021-05-04 08:54:05 +0700

    add getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,43 @@
-import path from "path";
-import { promises } from "fs";
+import path from 'path'
+import { promises } from 'fs'
+import Link from 'next/link'
 
 function HomePage(props) {
-  const { products } = props;
+  const { products } = props
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
-  );
+  )
 }
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await promises.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await promises.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    }
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true }
+  }
 
   return {
     props: {
       products: data.products,
     },
-  };
+    revalidate: 10,
+  }
 }
 
-export default HomePage;
+export default HomePage
